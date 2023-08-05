b92c493afd0adf520b007f544577b547bf15c46e
Adhiban05 <110900816+Adhiban05@users.noreply.github.com>
2023-08-05 16:41:32 +0530

    1

---
+++ 1.py
@@ -0,0 +1,33 @@
+class Person:
+    mylist = ["3", "5", "1"]
+
+    def operation(self, value):
+        if value == "1":
+            u = input("Enter the element to be added: ")
+            self.mylist.append(u)
+            print(self.mylist)
+        elif value == "2":
+            print(self.mylist)
+            e = input("Enter the element to be removed: ")
+            self.mylist.remove(e)
+            print(self.mylist)
+        elif value == "3":
+            print("Current list is: " + str(self.mylist))
+        elif value == "4":
+            self.mylist.sort()
+            print("Sorted list is: " + str(self.mylist))
+        elif value == "5":
+            d = len(self.mylist)
+            print("Length of the list is: " + str(d))
+        else:
+            print("Invalid case")
+
+    def run(self):
+        while True:
+            case = input("Enter a case (1-5) or any other key to exit: ")
+            self.operation(case)
+
+person = Person()
+person.run()
+
+                                        
